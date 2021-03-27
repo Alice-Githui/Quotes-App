387d83ec57599d8f6bdc021e74a2bd2f4c9337d0
Alice-Githui <gnyokabi.alice@gmail.com>
2021-03-27 16:31:34 +0300

    Create a custom pipe to return the number of days since the quote was created

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -11,8 +11,8 @@ export class QuotesComponent implements OnInit {
   quotes:QuotesList[] = [
     new QuotesList(1, 'We cannot become what we want by remaining what we are', 'Max Depree', 'Alice Githui', new Date(2021,2, 20)),
     new QuotesList(2, 'There is no greater agony than bearing an untold story inside you', 'Maya Angelou', 'Alice Githui', new Date(2021, 2, 26)),
-    new QuotesList(3, 'You cannot use up creativity. The more you use, the more you have', 'Maya Angelou', 'Alice Githui' , new Date(2021, 2, 27)),
-    new QuotesList(4, 'You have to be odd to be number one', 'Dr.Seuss', 'Alice Githui', new Date(2020, 2, 27))
+    new QuotesList(3, 'You cannot use up creativity. The more you use, the more you have', 'Maya Angelou', 'Alice Githui' , new Date(2021, 2, 25)),
+    new QuotesList(4, 'You have to be odd to be number one', 'Dr.Seuss', 'Alice Githui', new Date(2021, 2, 25))
 
   ]
 
