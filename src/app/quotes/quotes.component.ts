e4eca3f7a1b4bbb37778044d5180740c4450d74f
Alice-Githui <gnyokabi.alice@gmail.com>
2021-03-27 15:03:44 +0300

    Add the topnav image to the App

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -9,7 +9,7 @@ import {QuotesList} from '../quotes-list'
 export class QuotesComponent implements OnInit {
 
   quotes:QuotesList[] = [
-    new QuotesList(1, 'We cannot become what we want by remaining what we are', 'Max Depree', 'Alice Githui', new Date(2021,2, 26)),
+    new QuotesList(1, 'We cannot become what we want by remaining what we are', 'Max Depree', 'Alice Githui', new Date(2021,2, 20)),
     new QuotesList(2, 'There is no greater agony than bearing an untold story inside you', 'Maya Angelou', 'Alice Githui', new Date(2021, 2, 26)),
     new QuotesList(3, 'You cannot use up creativity. The more you use, the more you have', 'Maya Angelou', 'Alice Githui' , new Date(2021, 2, 27)),
     new QuotesList(4, 'You have to be odd to be number one', 'Dr.Seuss', 'Alice Githui', new Date(2020, 2, 27))
@@ -50,5 +50,6 @@ export class QuotesComponent implements OnInit {
 
   url="./assets/likebutton.jpg";
   lru = "./assets/dislikebutton.jpg";
+  url1 ="./assets/topnavimage.jpeg"
 
 }
